eeb114476792c5ea1a66388dcfd65887fc30f5a2
Fernanda Scheremetta <fernandascheremetta@gmail.com>
2025-07-08 11:03:53 -0300

    layout the select currency screen, font weight, stack alignment, .infinity, clipshape, order of modifiers

---
+++ LOTRConverter/SelectCurrency.swift
@@ -0,0 +1,54 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter
+//
+//  Created by Fernanda on 08/07/25.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            // Parchment background image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                // Text
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                
+                // currency icons
+                CurrencyIcon(currencyImage: .copperpenny, currencyName: "Copper Penny")
+                
+                // text
+                Text("Select the currency you would like to covert to:")
+                    .fontWeight(.bold)
+                
+                
+                // currency icons
+                
+                // done button
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}

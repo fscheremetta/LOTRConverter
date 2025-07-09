7bb8634463cd570ac2001ceba9369f86bc34fd25
Fernanda Scheremetta <fernandascheremetta@gmail.com>
2025-07-09 11:14:17 -0300

    onChange & sheet, decimal pad, xcode & app settings

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -68,12 +68,6 @@ struct ContentView: View {
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
                             .focused($leftTyping)
-                            .onChange(of: leftAmount) {
-                                if leftTyping {
-                                    rightAmount = leftCurrency
-                                        .convert(leftAmount, to: rightCurrency)
-                                }
-                            }
                     }
                     
                     // equal sign
@@ -108,17 +102,12 @@ struct ContentView: View {
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
                             .focused($rightTyping)
-                            .onChange(of: rightAmount) {
-                                if rightTyping {
-                                    leftAmount = rightCurrency
-                                        .convert(rightAmount, to: leftCurrency)
-                                }
-                            }
                     }
                 }
                 .padding()
                 .background(.black.opacity(0.5))
                 .clipShape(.capsule)
+                .keyboardType(.decimalPad)
                 
                 Spacer()
                 
@@ -138,6 +127,26 @@ struct ContentView: View {
                 }
             }
             // .border(.blue)
+        }
+        .onChange(of: leftAmount) {
+            if leftTyping {
+                rightAmount = leftCurrency
+                    .convert(leftAmount, to: rightCurrency)
+            }
+        }
+        .onChange(of: rightAmount) {
+            if rightTyping {
+                leftAmount = rightCurrency
+                    .convert(rightAmount, to: leftCurrency)
+            }
+        }
+        .onChange(of: leftCurrency) {
+            leftAmount = rightCurrency
+                .convert(rightAmount, to: leftCurrency)
+        }
+        .onChange(of: rightCurrency) {
+            rightAmount = leftCurrency
+                .convert(leftAmount, to: rightCurrency)
         }
         .sheet(isPresented: $showExchangeInfo) {
             ExchangeInfo()
